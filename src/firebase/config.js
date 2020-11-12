3d25674cf6b837f0db9819ac9b69f5a9f5da9bff
ss251 <ss251@uw.edu>
2020-11-12 08:07:46 +0530

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJwTlukccHHpYfNrK5BjyiHUQAfUgoVd8",
+  authDomain: "hunt-4186c.firebaseapp.com",
+  databaseURL: "https://hunt-4186c.firebaseio.com",
+  projectId: "hunt-4186c",
+  storageBucket: "hunt-4186c.appspot.com",
+  messagingSenderId: "845547000453",
+  appId: "1:845547000453:web:c39fc9f78ea6a18c09cfec",
+};
+
+export default firebaseConfig;
